9dc38c1a40b80559f094ed44b753de9253fdcdb9
codehz <git@hertz.moe>
2024-04-27 15:21:01 +0800

    use production branch

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,6 @@
 import { $ } from "bun";
 
 console.log('deploying pages...');
-await $`(cd .build && bun x wrangler pages deploy static)`;
+await $`(cd .build && bun x wrangler pages deploy static --branch production)`;
 console.log('deploying consumer...');
 await $`(cd consumer && bun x wrangler deploy)`;
